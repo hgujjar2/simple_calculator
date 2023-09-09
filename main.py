83d87ab247f5cad0a1e551e68e21209a57e426d1
hgujjar2 <f20barin1e02069@iub.edu.pk>
2023-09-10 03:20:47 +0500

    added main file

---
+++ main.py
@@ -0,0 +1,15 @@
+from addition import add
+    
+from multiplication import multiply
+from subtraction import subtract
+
+if __name__=="__main__":
+
+    a=6
+    b=4
+
+    print (add(a,b))
+    
+    print (multiply(a,b))
+    print (subtract(a,b))
+    
